a04619e77ee293957b9580b10c83ddd297a4dd46
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-04-19 23:52:44 +0700

    recover

--- pages/api/daily.js
+++ pages/api/daily.js
@@ -6,11 +6,11 @@ module.exports = async (req, res) => {
     return {
       positive: item.jumlah_positif.value,
       active: item.jumlah_dirawat.value,
-      cured: item.jumlah_sembuh.value,
+      recover: item.jumlah_sembuh.value,
       death: item.jumlah_meninggal.value,
       positive_cumulative: item.jumlah_positif_kum.value,
       active_cumulative: item.jumlah_dirawat_kum.value,
-      cured_cumulative: item.jumlah_sembuh_kum.value,
+      recover_cumulative: item.jumlah_sembuh_kum.value,
       death_cumulative: item.jumlah_meninggal_kum.value,
       last_update: item.key,
       datetime: item.key_as_string,
